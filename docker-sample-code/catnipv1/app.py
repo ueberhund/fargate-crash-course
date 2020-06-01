1e7690004cd525a79433fa05ae467d63ae8f6737
George <gmike@a483e7ce1810.ant.amazon.com>
2020-06-01 14:07:07 -0600

    Adding files

---
+++ docker-sample-code/catnipv1/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+import os
+import random
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
