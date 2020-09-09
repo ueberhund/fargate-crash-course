cff8862d9d233472c0c5169df78e60d7b1027cb1
Mike <ueberhund@hotmail.com>
2020-09-09 15:41:32 -0600

    adding files

---
+++ Extra_Credit/Prometheus/prometheus-install.sh
@@ -0,0 +1,20 @@
+export AWS_PROFILE=<profile>
+export AWS_DEFAULT_REGION=<region>
+export ECS_CLUSTER_NAME=<cluster name>
+export ECS_LAUNCH_TYPE=FARGATE
+export CREATE_IAM_ROLES=True
+export ECS_CLUSTER_SECURITY_GROUP=<security group id>
+export ECS_CLUSTER_SUBNET=<subnet id>
+
+aws cloudformation create-stack --stack-name CWAgent-Prometheus-ECS-${ECS_CLUSTER_NAME}-${ECS_LAUNCH_TYPE}-awsvpc \
+    --template-body file://cwagent-ecs-prometheus-metric-for-awsvpc.yaml \
+    --parameters ParameterKey=ECSClusterName,ParameterValue=${ECS_CLUSTER_NAME} \
+                 ParameterKey=CreateIAMRoles,ParameterValue=${CREATE_IAM_ROLES} \
+                 ParameterKey=ECSLaunchType,ParameterValue=${ECS_LAUNCH_TYPE} \
+                 ParameterKey=SecurityGroupID,ParameterValue=${ECS_CLUSTER_SECURITY_GROUP} \
+                 ParameterKey=SubnetID,ParameterValue=${ECS_CLUSTER_SUBNET} \
+                 ParameterKey=TaskRoleName,ParameterValue=CWAgent-Prometheus-TaskRole-${ECS_CLUSTER_NAME} \
+                 ParameterKey=ExecutionRoleName,ParameterValue=CWAgent-Prometheus-ExecutionRole-${ECS_CLUSTER_NAME} \
+    --capabilities CAPABILITY_NAMED_IAM \
+    --region ${AWS_DEFAULT_REGION} \
+    --profile ${AWS_PROFILE}
